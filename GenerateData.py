04534c9ffbb36d70023aff63a68c10c4c78f691b
reyniersquillace <reysquillace@gmail.com>
2024-05-08 14:59:43 -0400

    Edits to GenerateData

--- GenerateData.py
+++ GenerateData.py
@@ -1,20 +1,19 @@
 import numpy as np
 import numpy.random as rand
-import Princeton
 
 def lorentzian(phi, gamma, x0 = 512):
     return (gamma/((phi - x0)**2 + gamma**2))/np.pi
 
 
-def generate(n_samples, n_bins, noise = True):
-    max_subpulses = 6
+def generate(n_samples, n_bins, noise = False):
+    max_subpulses = 4
     phi = np.arange(n_bins, dtype = int)
     pulses = np.zeros((n_samples, n_bins))
     locs = np.zeros((n_samples, n_bins)) 
     gammas = np.copy(locs)
 
     for i in range(n_samples):
-        gamma = rand.choice(np.linspace(1, 50, 1024))
+        gamma = rand.choice(np.linspace(1, 50, n_bins))
         pulse = lorentzian(phi, gamma)
         pulse /= max(pulse)
         pulse = np.zeros(1024)
@@ -23,7 +22,7 @@ def generate(n_samples, n_bins, noise = True):
         subpulses = max_subpulses 
         #subpulses
         for j in range(subpulses):
-            gamma_sub = rand.choice(np.linspace(1, 50, 1024))
+            gamma_sub = rand.choice(np.linspace(1, 50, n_bins))
             loc_sub = 512 + rand.randint(-200, 200)
             subpulse = lorentzian(phi, gamma_sub,loc_sub)
             amp = rand.choice(np.linspace(0.1, 0.9))
@@ -34,15 +33,14 @@ def generate(n_samples, n_bins, noise = True):
             gammas[i, j + 1] = gamma_sub
 
         if noise:
-            rms = rand.choice(np.linspace(0.001, 0.3, 3000))
+            rms = rand.choice(np.linspace(0.001, 0.1, 3000))
             pulse += rand.normal(0, rms, n_bins)
 
         pulses[i] = pulse
         locs[i, 0] = 512
         gammas[i, 0] = gamma
-        amps[i, 0] = 1
 
-    return pulses, locs, gammas, amps
+    return pulses, locs, gammas
 
 def generate_dummy(n_samples, n_bins):
     phi = np.linspace(0, 1, 1024)
